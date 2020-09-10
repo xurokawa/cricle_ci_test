e9f459cf5dbd983dfbe22c69b4ac33dda8b4b6fe
fumiaki.mizuno <fumiaki.mizuno@supership.jp>
2020-09-10 10:35:57 +0900

    fixed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validation' do
     describe 'validation' do
-      it { is_expected.not_to validate_presence_of :name }
+      it { is_expected.to validate_presence_of :name }
       it { is_expected.to validate_presence_of :email }
       it { validate_uniqueness_of(:email) }
     end
